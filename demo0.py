653e1767a75b5e42cd6bd69b4abd74b0764ee6ce
pferney05 <164430613+pferney05@users.noreply.github.com>
2024-09-04 15:16:26 -0600

    Update demo0.py

--- demo0.py
+++ demo0.py
@@ -12,14 +12,16 @@ startingQuantity = {
 flux = batman.Flux(fluxes=0.)
 chain = batman.Chain()
 
-seq = batman.Sequence(chain,False)
-seq.set_init_quantity(startingQuantity)
-seq.add_initial()
+time = np.array([0.,1e17])
+power = np.array([0.,0.])
+prob = batman.Continuous(chain, time, power, startingQuantity, fission=False)
 
 tpoints = np.logspace(0,17,num=100)
 nuclideList = ['U238','U234','Th230','Pb206','U235','Pa231','Pb207']
 
-seq.plot_quantity(tpoints,nuclideList,ymax=10**24,xlogScale=True,cmap='jet',xscale=3600.*24*365.25)
-seq.plot_activity(tpoints,nuclideList,ymin=10**-12,ymax=10**9,xlogScale=True,cmap='jet',xscale=3600.*24*365.25)
+activity, totalActivity = prob.solve_activity(tpoints)
+
+prob.plot_quantity(tpoints,nuclideList,ymax=10**24,title='Qty vs. Time',xLabel='Time(years)',yLabel='Quantity (atoms)',xlogScale=True,cmap='jet',xscale=3600.*24*365.25)
+prob.plot_activity(tpoints,nuclideList,ymin=10**-12,title='Activity vs. Time',xLabel='Time(years)',yLabel='Activity (Bq)',ymax=10**9,xlogScale=True,cmap='jet',xscale=3600.*24*365.25)
 
 batman.Sequence.show()
